358520b25c084ea6cf47cd31f1f95e933d143940
Alessandro <alessandro.mancinelli89@gmail.com>
2020-11-16 16:07:47 +0100

    Done

--- js/main.js
+++ js/main.js
@@ -1,9 +1,15 @@
+/*
+Utilizzando il framework Vue
+ - Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
+ - Aggiungere un’immagine presa anch’essa da un data.
+*/
+
 var app = new Vue({
   el:'#root',
   data: {
     name: 'Google',
     year: 1998,
     founders: ['Larry Page', 'Sergey Brin'],
-    logo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/aa/Logo_Google_2013_Official.svg/1024px-Logo_Google_2013_Official.svg.png' 
+    logo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/aa/Logo_Google_2013_Official.svg/1024px-Logo_Google_2013_Official.svg.png'
   }
 });
