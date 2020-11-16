cc99bf539afab0c8a8404c805181ddc1d96af9a1
Alessandro <alessandro.mancinelli89@gmail.com>
2020-11-16 12:55:51 +0100

    Done

---
+++ js/main.js
@@ -0,0 +1,9 @@
+var app = new Vue({
+  el:'#root',
+  data: {
+    name: 'Google',
+    year: 1998,
+    founders: ['Larry Page', 'Sergey Brin'],
+    logo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/aa/Logo_Google_2013_Official.svg/1024px-Logo_Google_2013_Official.svg.png' 
+  }
+});
